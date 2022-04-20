a76d50ab54b0e10b8bab904995a3fbb507ab9b60
magio843 <magio843@yandex.ru>
2022-04-20 20:35:40 +0700

    Решение задачи

--- Finaltask/Program.cs
+++ Finaltask/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//
+string[] array = new string[10] {"hellow", "9", "gel", "send", "com", "son1", "23fin","coma", "-1","1111"};
+string[] arraynew = new string[array.Length];
+void ArrayResul(string[] array, string[] arraynew)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+       if(array[i].Length <= 3)
+        {
+        arraynew[count] = array[i];
+        System.Console.Write($"{arraynew[count]};  ");
+        count++;
+        }
+    }
+}
+
+ArrayResul(array, arraynew);
+
+
+
